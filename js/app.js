179d96741a6eec0fec9f1b5e087461c0ebe0a455
Martin Tudor <martcan27@gmail.com>
2018-03-21 18:58:39 +0000

    Create a list (an array) that holds all of your cards (icons)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const array = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle"];
 
 /*
  * Display the cards on the page
