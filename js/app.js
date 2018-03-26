44335f144f190597f1e6f48c9c6250fc0b3e368b
Martin Tudor <martcan27@gmail.com>
2018-03-26 21:15:50 +0100

    refactor: Try to fix bug
    I'm trying to fix a bug where a card very occassionally remains open when it should close.
    So far, I'm without success.

--- js/app.js
+++ js/app.js
@@ -56,7 +56,7 @@ function cardClick() {
     let i;
     for (i = 0; i < 16; i += 1) {
         const card = document.querySelectorAll('.card')[i];
-        card.addEventListener('click', displaySymbol, true);
+        card.addEventListener('click', displaySymbol);
         // run displaySymbol function when card is clicked
         //true - The event handler is executed in the capturing phase
     }
@@ -73,6 +73,7 @@ function displaySymbol(event) {
     "use strict";
     const eventTarget = event.target;
     eventTarget.classList.add("open", "show");
+    eventTarget.removeEventListener('click', displaySymbol);
     // call move counter function
     moveCounter();
     whatSymbol(eventTarget);
@@ -96,13 +97,13 @@ function holdCards(icon, eventTarget) {
     openCardsArr.push(eventTarget);
     //check to see if cards match
     if (openCardsArr.length !== 4) {
-        eventTarget.removeEventListener('click', displaySymbol, true); // Make sure user can't match by clicking on the same square
+        // openCardsArr[1].eventTarget.removeEventListener('click', displaySymbol, true); // Make sure user can't match by clicking on the same square
         return;
     }
     if (openCardsArr[0] === openCardsArr[2]) {
         matchCards();
     } else {
-        openCardsArr[1].addEventListener('click', displaySymbol, true); // Reintroduce event listener. See above.
+        // openCardsArr[1].addEventListener('click', displaySymbol, true); // Reintroduce event listener. See above.
         closeCards();
     }
 
@@ -117,8 +118,8 @@ function matchCards() {
     eventTarget1.classList.add("match");
     eventTarget2.classList.remove("show");
     eventTarget2.classList.add("match");
-    eventTarget1.removeEventListener('click', displaySymbol, true);
-    eventTarget2.removeEventListener('click', displaySymbol, true);
+    eventTarget1.removeEventListener('click', displaySymbol);
+    eventTarget2.removeEventListener('click', displaySymbol);
     openCardsArr = [];
 
 }
@@ -127,17 +128,23 @@ function closeCards() {
 
     let eventTarget1 = openCardsArr[1];
     let eventTarget2 = openCardsArr[3];
+
+
     eventTarget1.id = "incorrect";
     eventTarget2.id = "incorrect";
-    setTimeout(pause, 1000)
+    setTimeout(pause, 200);
 
     function pause() {
-        eventTarget1.className = "card";
-        eventTarget2.className = "card";
-        openCardsArr = [];
         eventTarget1.id = "";
         eventTarget2.id = "";
+        eventTarget1.className = "card";
+        eventTarget2.className = "card";
+        eventTarget1.addEventListener('click', displaySymbol);
+        eventTarget2.addEventListener('click', displaySymbol);
+        openCardsArr = [];
+
     }
+
 }
 
 
